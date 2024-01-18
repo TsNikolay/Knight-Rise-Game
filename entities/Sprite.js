356394e666d22f98dbe543fd94eee325466b0e48
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2024-01-18 21:41:45 +0100

    Bug with loading pictures: Second attempt to solve it

--- entities/Sprite.js
+++ entities/Sprite.js
@@ -1,14 +1,14 @@
 import { context } from "../main.js";
 
 export class Sprite {
-  constructor(x, y) {
+  constructor(imgSrc, x, y) {
     this.x = x;
     this.y = y;
     this.image = new Image();
     this.image.onload = () => {
       this.isLoaded = true;
     };
-    this.image.src = "./data/yard.png";
+    this.image.src = imgSrc;
     this.isLoaded = false;
   }
 
